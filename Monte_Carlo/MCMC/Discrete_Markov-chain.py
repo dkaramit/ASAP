ee90b6dcce0ad97780b748dbf051f8f0ea83e96a
dimitronic <dkaramit@yahoo.com>
2018-06-19 18:45:07 +0300

    Generalized the Discrete Markov Chain. Now the input is just the number of steps and the transition matrix.

--- Monte_Carlo/MCMC/Discrete_Markov-chain.py
+++ Monte_Carlo/MCMC/Discrete_Markov-chain.py
@@ -1,25 +1,32 @@
 import numpy as np
 
 '''
-The trasition matrix defines the Markov Chain. If you change to higher dimentions, then you should change the dimension of variables
-v and state, accordingly.
+The trasition matrix defines the Markov Chain. If you change to higher dimentions and want to start from
+a state diferent from 0, then you should change the dimension of variables v and state, accordingly.
 '''
 Transition=np.array([\
-            [0.5,0.3,0.2],\
-            [0.05,0.45,0.5],\
-            [0.25,0.35,0.4],\
+            [0.5,0.3,0.1,0.1],\
+            [0.05,0.45,0.4,0.1],\
+            [0.25,0.35,0.3,0.1],\
+            [0.4,0.1,0.25,0.25]
             ])#Reminder: the sum of each row sums up to 1.
 
-#number of steps in both the Iteration and simulation.
-N_tot=25000
+#Number of steps in both the Iteration and Simulation.
+N_tot=250000
 
+#Doing the following you start at state 0 automatically.
+len_T=len(Transition[0])
+init_s=np.zeros(len_T)
+init_s[0]=1
 
 
 '''
 Iterative solution to w.P=w.
 Iteratively find w^{(n)}=w^{(0)}.P^{(n+1)}.
 '''
-v=[1,0,0]#play around with this (the final result should not change as long as the v.v=1)
+#v=[1,0,0,0]#play around with this (the final result should not change as long as the v.v=1)
+v=init_s[:]#start at state 0 automatically
+
 
 s=v[:]
 for i in np.arange(N_tot):
@@ -34,9 +41,9 @@ Simulation.
 Simulate the Markov Chain
 '''
 
-state=[1,0,0]#This is the initial sate vector, which indicates the current state. e.g [1,0,0] indicates that the system is in state 0 (I start counting from 0).
+#state=[1,0,0,0]#This is the initial sate vector, which indicates the current state. e.g [1,0,0] indicates that the system is in state 0 (I start counting from 0).
 
-
+state=init_s[:]#start at state 0 automatically
 def Transition_Probability(state,Transition):
     _probabilities=np.dot(Transition.T,state)#probabilities to move from the current state to all other states.
     return _probabilities
@@ -64,6 +71,15 @@ print 'simulation=', np.array(_visits)/float(_N)#probabilities is the fraction o
 
 
 print "As for large N_tot, the simulated result should approach the iteratitve one."
+
+
+
+
+
+
+
+
+
 
 
 
