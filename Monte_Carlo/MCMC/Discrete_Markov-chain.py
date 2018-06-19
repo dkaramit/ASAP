1b42e296cf43e1b49da8b852c300969f5ba22a80
dimitronic <dkaramit@yahoo.com>
2018-06-19 18:25:56 +0300

    Typos in comments corrected

--- Monte_Carlo/MCMC/Discrete_Markov-chain.py
+++ Monte_Carlo/MCMC/Discrete_Markov-chain.py
@@ -1,29 +1,28 @@
 import numpy as np
 
 '''
-The trasition matrix defines the Markov Chain. If you change to higher dimentions, then you should change the variables:
-i) v
-ii) state
-
+The trasition matrix defines the Markov Chain. If you change to higher dimentions, then you should change the dimension of variables
+v and state, accordingly.
 '''
 Transition=np.array([\
             [0.5,0.3,0.2],\
             [0.05,0.45,0.5],\
             [0.25,0.35,0.4],\
-            ])
+            ])#Reminder: the sum of each row sums up to 1.
 
+#number of steps in both the Iteration and simulation.
+N_tot=25000
 
 
 
 '''
-Iterative solution to w.P=w
+Iterative solution to w.P=w.
 Iteratively find w^{(n)}=w^{(0)}.P^{(n+1)}.
 '''
-v=[0.1,0.1,0.8]#play around with this (the final result should not change as long as the v.v=1)
+v=[1,0,0]#play around with this (the final result should not change as long as the v.v=1)
 
 s=v[:]
-N_tot=100000
-for i in range(N_tot):
+for i in np.arange(N_tot):
     s=np.dot(Transition.T,s)
     #print s
 print "iterative=", s
@@ -44,8 +43,9 @@ def Transition_Probability(state,Transition):
 
 
 _N=0
+_visits=np.zeros(len(state))#initialize the number of visits.
 
-_visits=np.zeros(len(state))
+
 while True:
     if _N==N_tot:
         break
@@ -59,7 +59,7 @@ while True:
     _N+=1
 
 
-print 'simulation=', np.array(_visits)/float(_N)#probabilities is ne fraction of visits over time.
+print 'simulation=', np.array(_visits)/float(_N)#probabilities is the fraction of visits over time.
 
 
 
