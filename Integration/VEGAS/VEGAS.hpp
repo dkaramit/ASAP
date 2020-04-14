1725ac679a94f6157dba45b018821ae2f5acf2e6
dkarmit <dkaramit@uahoo.com>
2020-04-14 04:56:08 +0200

    VEGAS in C++ started. Url resource added.

--- Integration/VEGAS/VEGAS.hpp
+++ Integration/VEGAS/VEGAS.hpp
@@ -1,3 +1,6 @@
+//  Would be helpful to read:
+//  https://www.ippp.dur.ac.uk/~krauss/Lectures/QuarksLeptons/Basics/PS_Vegas.html
+
 #include"VEGAS_class.hpp"
 #include"VEGAS_const.hpp"
 #include"VEGAS_aux.hpp"
