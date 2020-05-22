464c7e1c3ca73ceb5790f7ce2f0578066d3fc287
dkarmit <dkaramit@uahoo.com>
2020-05-22 20:20:03 +0200

    still testiing SA for ODEs.

--- Monte_Carlo/Simulated-Annealing/python/SimulatedAnnealing.py
+++ Monte_Carlo/Simulated-Annealing/python/SimulatedAnnealing.py
@@ -1,17 +1,26 @@
 import numpy as np
 
-
 class SimulatedAnnealing:
-    def __init__(self, func, dim , x0 ,  T0, k, IterationT, MinT, sigma, tol, Nstar):
+    def __init__(self, func, dim , x0 ,  T0, k, IterationT, MinT, sigma, tol, Nstar, p0,N0,k0):
         '''
         func: the function to be minimized
+        
         dim: the number of arguments that func takes
+        
         x0: starting point
+        
         k: to be used for the temperature update
+        
         IterationT: number of iterations per temperature
+        
         MinT:  stop when the temperature becomes MinT
-        sigma: new neighbours are found between x+-sigma/2
-        tol,Nstar: SA Stops when |\Delta E |<tol for Nstar successive iterations
+        sigma: new neighbours are found between x+N(0,sigma)
+        
+        tol,Nstar: SA Stops when the acceptance probability is below tol for Nstar successive iterations
+        
+        To initialize the temperature:
+        p0,N0,k0: increase the temperature as T->T*k0 until the acceptance probability is larger that p0
+        for N0 successive iterations
         '''
         
         self.func=func
@@ -25,6 +34,10 @@ class SimulatedAnnealing:
         self.sigma=sigma
         self.tol=tol
         self.Nstar=Nstar
+        
+        self.p0=p0
+        self.N0=N0
+        self.k0=k0
         
         self.E=self.func(x0)
             
@@ -43,10 +56,12 @@ class SimulatedAnnealing:
         '''Given a new value of the energy, return the Boltzmann factor'''
         return np.exp(-(Enew-self.E)/self.T)
     
+        
+    
+    
     def runT(self):
         #use these to find the mean Delta E for a temperature 
-        self.DE=0
-        acc=0
+        self.AccProb=0
         
         for _ in range(self.IterationT):
             xnew=self.PickNeighbour()
@@ -54,42 +69,59 @@ class SimulatedAnnealing:
             
             
             if Enew<self.E or self.BoltzmannP(Enew) > np.random.rand():
-                self.DE+=Enew
-                acc+=1
-                
+                self.AccProb+=1
                 self.E=Enew
                 self.x=xnew
                 
+        self.AccProb=self.AccProb/self.IterationT
+
         
-        if acc==0:
-            self.DE=0
-        else:
-            self.DE=np.abs(self.DE/acc-self.E) 
+    def InitT(self):
+        '''Find an appropriace initial temperature'''
+        IterT0=0
+        
+        while IterT0<self.N0:
+            self.runT()
+           
+            if self.AccProb>self.p0:
+                IterT0+=1
+            
+            if self.AccProb<self.p0 and IterT0>0:
+                IterT0=0
+            
+            self.T*=self.k0
                 
+
+        
+        
     def run(self, CList=False):
         '''
-        Iterate until the temperature reaches MinT or until it reaches convergence
-        CList=True stores E and IterConv values for all temperatures in self.ListIC and self.ListE
+        Iterate until the temperature reaches MinT or until it reaches convergence (Nstart times with AccProb<tol)
+        CList=True stores acceptance probabilities, IterConv, E values for all temperatures 
+        in self.ListProb self.ListIC, self.ListE
         '''
-        
+        #run to find initial temperature that has large acceptance probability 
+        self.InitT()
         
         IterConv=0
         
         if CList:
+            self.ListProb=[]
             self.ListIC=[]
             self.ListE=[]
             
         while self.T>self.MinT and self.Nstar>IterConv:
             self.runT()
 
-            if self.DE<self.tol:
+            if self.AccProb<self.tol:
                 IterConv+=1
             
-            if self.DE>self.tol and IterConv>0:
+            if self.AccProb>self.tol and IterConv>0:
                 IterConv=0
             
             
             if CList:
+                self.ListProb.append(self.AccProb)
                 self.ListIC.append(IterConv)
                 self.ListE.append(self.E)
             
